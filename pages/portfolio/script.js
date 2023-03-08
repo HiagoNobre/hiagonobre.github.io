0dedbbe819d45f9db65b5075c7ad662144808e4d
Hiago <nobrehiago21@hotmail.com>
2023-03-08 11:11:48 -0300

    add event listener

---
+++ pages/portfolio/script.js
@@ -0,0 +1,7 @@
+const linkHome = 'https://hiagonobre.github.io/';
+
+const linkToHome = document.querySelector('.link-to-home');
+
+linkToHome.addEventListener('click', () => {
+  window.location.href = 'https://hiagonobre.github.io/';
+});
