609de4e8eedbbede09313628b6c3d639e297a006
Hiago <nobrehiago21@hotmail.com>
2023-03-08 11:01:36 -0300

    Update script.js

--- pages/contato/script.js
+++ pages/contato/script.js
@@ -3,5 +3,6 @@ const linkHome = 'https://hiagonobre.github.io/';
 const linkToHome = document.querySelector('.link-to-home');
 
 linkToHome.addEventListner('click', () => {
-  Window.location.href = 'https://hiagonobre.github.io/';
+  window.location.href = 'https://hiagonobre.github.io/';
+  console.log('passou')
 });
